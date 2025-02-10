be3ea22efe09cbab0a509498dfdc7edf3c908766
Oglama <181064963+oglama@users.noreply.github.com>
2025-02-10 12:23:30 +0200

    [update] Version 0.0.42
    # February 10, 2025
    [code] Improved 500 page

---
+++ dist/ssg/_next/static/WIiX-GKMVGXa7qTMJ0MbM/_buildManifest.js
@@ -0,0 +1,17 @@
+/*!
+ * @architect Mark Jivko <mark@oglama.com>
+ * @copyright © 2024-2025 Oglama https://oglama.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+self.__BUILD_MANIFEST=function(s){return{__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":[s,"static/chunks/688-02f7baae547adc58.js","static/chunks/pages/index-6cf9f98a2771a5d0.js"],"/401":["static/chunks/pages/401-83d7d5cf2d8e5ac4.js"],"/404":["static/chunks/pages/404-21c481ca292ce4aa.js"],"/500":["static/chunks/pages/500-38ec18e134d5f66b.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],"/_offline":["static/chunks/pages/_offline-34198a61a2499178.js"],"/login":[s,"static/chunks/pages/login-50fd9be11d44d71a.js"],"/source-code":["static/chunks/pages/source-code-f516beb6fc04b898.js"],sortedPages:["/","/401","/404","/500","/_app","/_error","/_offline","/login","/source-code"]}}("static/chunks/118-8cb12da1a7588de5.js"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
