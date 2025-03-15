934bb0208563b78fd6f896e11659f96cf060e55d
Oglama <181064963+oglama@users.noreply.github.com>
2025-03-15 15:41:18 +0200

    [update] Version 0.0.57
    # March 15, 2025
    [chore] Fixed typo
    [code] Fixed source code outputs interaction w/ results page
    [code] Fixed diskStorage.outputsQuery redundant runs
    [code] Added TabResultsFilterInt
    [code] Added string filter highlights
    [code] Added TabResultsFilterString
    [code] Fixed reloading bugs
    
    # March 14, 2025
    [chore] Modified OUTPUTS_QUERY_LIMIT
    [code] Added TabResultsItemString
    [code] Switch TabResultsItemBool to a BarChart
    [code] Added TabResultsItemInt
    [code] Added TabResultsItemFiles
    [code] Improved runs UI
    [code] TabResultsRuns
    [code] Global Context filters for runs and fields
    
    # March 13, 2025
    [code] Used global context for output query filters
    [code] TabResultsItemBool
    [code] Improved consistency between settings and results
    
    # March 12, 2025
    [code] TabResultsItem baby steps
    [code] Custom summary icon for SourceCodeIoItem
    [code] Fixed default state change warnings
    [code] SoC for inputsStore
    
    # March 11, 2025
    [code] Improved diskStorage IO
    
    # March 10, 2025
    [code] Improved saveUrl
    
    # March 9, 2025
    [code] diskStorage.outputsQuery()
    [code] More intuitive $.io SDK
    
    # March 8, 2025
    [code] Cleaner IO, smaller memory footprint

---
+++ dist/ssg/_next/static/chunks/pages/404-38903a01bfc8d823.js
@@ -0,0 +1,17 @@
+/*!
+ * @architect Mark Jivko <mark@oglama.com>
+ * @copyright © 2024-2025 Oglama https://oglama.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[197],{20394:function(n,e,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return t(92090)}])},92090:function(n,e,t){"use strict";t.r(e);var r=t(14924),i=t(85893),o=t(77058),s=t(90948),a=t(15861),u=t(80822),c=t(40045),f=(0,s.ZP)(u.Z)((function(n){var e=n.theme;return(0,r.Z)({},e.breakpoints.down("md"),{width:"90vw"})})),l=(0,s.ZP)("img")((function(n){return{marginTop:n.theme.spacing(10)}}));e.default=function(n){var e=n.title,t=n.message,r=n.sx,s=void 0===r?{}:r,h=(0,o.$)().i18n;return(0,i.jsxs)(u.Z,{className:"content-center",sx:s,children:[(0,i.jsxs)(u.Z,{sx:{display:"flex",flexDirection:"column",alignItems:"center",textAlign:"center"},children:[(0,i.jsxs)(f,{children:[!1!==e?(0,i.jsx)(a.Z,{variant:"h1",sx:{"&.MuiTypography-root":{fontSize:"3rem",fontWeight:200,textTransform:"uppercase"}},children:"undefined"!==typeof e?e:h.t("Error 404")}):null,!1!==t?(0,i.jsx)(a.Z,{variant:"h5",sx:{"&.MuiTypography-root":{fontSize:"1.2rem !important",fontWeight:400}},children:"undefined"!==typeof t?t:h.t("Page not found")}):null]}),(0,i.jsx)(l,{alt:"error-illustration",height:"350",src:"/img/pages/page-404.png"})]}),(0,i.jsx)(c.Z,{})]})}}},function(n){n.O(0,[774,888,179],(function(){return e=20394,n(n.s=e);var e}));var e=n.O();_N_E=e}]);
