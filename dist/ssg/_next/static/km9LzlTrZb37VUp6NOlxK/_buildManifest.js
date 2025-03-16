1338534090226fa3b798f9b3a00c69ba52f6ec57
Oglama <181064963+oglama@users.noreply.github.com>
2025-03-16 09:04:03 +0200

    [update] Version 0.0.59
    # March 16, 2025
    [code] CodeEditor MacOS shortcuts
    [code] CodeEditor - enabled JS formatting

---
+++ dist/ssg/_next/static/km9LzlTrZb37VUp6NOlxK/_buildManifest.js
@@ -0,0 +1,17 @@
+/*!
+ * @architect Mark Jivko <mark@oglama.com>
+ * @copyright © 2024-2025 Oglama https://oglama.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+self.__BUILD_MANIFEST=function(s,c,e){return{__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":[s,c,"static/chunks/422-00d79d5194d753a1.js",e,"static/chunks/pages/index-19751c7bea3503be.js"],"/401":["static/chunks/pages/401-83d7d5cf2d8e5ac4.js"],"/404":["static/chunks/pages/404-38903a01bfc8d823.js"],"/500":["static/chunks/pages/500-71919e04c7ffefc3.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],"/_offline":["static/chunks/pages/_offline-34198a61a2499178.js"],"/login":[s,"static/chunks/pages/login-8d3c376499a13824.js"],"/source-code":[s,c,"static/chunks/747-67065d463ed717d0.js",e,"static/chunks/pages/source-code-df4ffb85417646d0.js"],sortedPages:["/","/401","/404","/500","/_app","/_error","/_offline","/login","/source-code"]}}("static/chunks/29-cb7a17a12e23da6b.js","static/chunks/821-7383b3be271808d7.js","static/chunks/870-b3c60c47c166afa8.js"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
