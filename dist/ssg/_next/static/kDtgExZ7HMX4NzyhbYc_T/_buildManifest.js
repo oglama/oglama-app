721d21a0df2e9f5faa2991c03887dbb222924f71
Oglama <181064963+oglama@users.noreply.github.com>
2025-04-01 10:34:49 +0300

    [update] Version 0.0.67
    # April 1, 2025
    [code] esbuild
    [code] Added <TabResultsItemTable/>
    
    # March 31, 2025
    [code] Added $.ioInputRow, $.ioOutputRow
    [code] Removed zombie tables
    [code] Improved diskStorage source cache
    [code] Fixed TableField event bubbling
    [code] CRUD on input <TableField/>
    
    # March 30, 2025
    [code] Table size restrictions
    [code] Run ID sanitization
    [code] Exposed diskStorage.[input|output]Table* methods to preloader
    [code] diskStorage.outputTableAdd
    
    # March 29, 2025
    [code] Implemente diskStorage table methods inputTableQuery, inputTableAdd, inputTableGet, inputTableUpdate, inputTableDelete, outputTableQuery
    
    # March 28, 2025
    [code] diskStorage:[input/output]Table* scaffolding
    
    # March 26, 2025
    [code] Table field - UI elements
    
    # March 25, 2025
    [code] Table columns UI
    [code] Baby steps for <TableField />
    [code] Linting and typos
    [code] Unified source code limits
    [chore] Linting
    [code] Cleaner <ChipField/> integration
    
    # March 24, 2025
    [code] Prepared <SortableChips/>
    [code] Tables - baby steps
    
    # March 23, 2025
    [code] Logs Scroll styling
    [chore] Request metod styling

---
+++ dist/ssg/_next/static/kDtgExZ7HMX4NzyhbYc_T/_buildManifest.js
@@ -0,0 +1,17 @@
+/*!
+ * @architect Mark Jivko <mark@oglama.com>
+ * @copyright © 2024-2025 Oglama https://oglama.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+self.__BUILD_MANIFEST=function(s,c,e){return{__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":[s,c,"static/chunks/401-47fc40c31b5673ab.js",e,"static/chunks/pages/index-4328ccf84a856297.js"],"/401":["static/chunks/pages/401-83d7d5cf2d8e5ac4.js"],"/404":["static/chunks/pages/404-38903a01bfc8d823.js"],"/500":["static/chunks/pages/500-71919e04c7ffefc3.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],"/_offline":["static/chunks/pages/_offline-34198a61a2499178.js"],"/login":[s,"static/chunks/pages/login-4dabdf29a066c630.js"],"/source-code":[s,c,"static/chunks/7-3c31d3ef8a07f7a3.js",e,"static/chunks/pages/source-code-dc1a7676df7b68c2.js"],sortedPages:["/","/401","/404","/500","/_app","/_error","/_offline","/login","/source-code"]}}("static/chunks/693-ea892adf950891fa.js","static/chunks/255-501d050b0ebf28b6.js","static/chunks/870-44eeb050ff1d91ee.js"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
