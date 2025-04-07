61e6b204728dc7f87f1c30e4d2ebc2b5c0e25381
Oglama <181064963+oglama@users.noreply.github.com>
2025-04-07 11:46:40 +0300

    [update] Version 0.0.73
    # April 7, 2025
    [code] Improved AbortController
    [code] Agent play button hidden
    [code] Navigate to settings after app install
    
    # April 6, 2025
    [code] Implemented $.fn
    [chore] Removed deprecated fn calls
    [code] Replaced .action with .code
    [code] AgentBanner re-render
    [code] Added message to $.pause
    
    # April 5, 2025
    [code] Fixed devmode exit on windows
    
    # April 4, 2025
    [chore] Updated labels
    
    # April 3, 2025
    [code] Fixed input init bug

---
+++ dist/ssg/_next/static/GYDb91rM2QAuOzpS3gVzb/_buildManifest.js
@@ -0,0 +1,17 @@
+/*!
+ * @architect Mark Jivko <mark@oglama.com>
+ * @copyright © 2024-2025 Oglama https://oglama.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+self.__BUILD_MANIFEST=function(s,c,e){return{__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":[s,c,"static/chunks/752-629e4f5c4ef1469b.js",e,"static/chunks/pages/index-ce162630ebe20eaa.js"],"/401":["static/chunks/pages/401-83d7d5cf2d8e5ac4.js"],"/404":["static/chunks/pages/404-38903a01bfc8d823.js"],"/500":["static/chunks/pages/500-71919e04c7ffefc3.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],"/_offline":["static/chunks/pages/_offline-34198a61a2499178.js"],"/login":[s,"static/chunks/pages/login-ebefe51b9269caeb.js"],"/source-code":[s,c,"static/chunks/535-77984546051925ae.js",e,"static/chunks/pages/source-code-640096aa230eff27.js"],sortedPages:["/","/401","/404","/500","/_app","/_error","/_offline","/login","/source-code"]}}("static/chunks/693-ea892adf950891fa.js","static/chunks/605-bc02aa28f4e774e7.js","static/chunks/629-7348ac1432ed8e14.js"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
