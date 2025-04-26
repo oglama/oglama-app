3cd20273e28d8417a3df279b0c6c24ae82b19ff0
Oglama <181064963+oglama@users.noreply.github.com>
2025-04-26 11:25:08 +0300

    [update] Version 0.0.77
    # April 26, 2025
    [chore] Agent tab styling
    [code] Added navigation bar
    [code] Improved Agent Nav
    
    # April 25, 2025
    [code] URL bar baby steps
    [code] Cleaner typing
    
    # April 24, 2025
    [code] Improved box model
    
    # April 23, 2025
    [code] Updated limits
    [code] Updated $.doHighlightBox
    [code] Updated labels
    [code] Allowing $.pause, $.stop within $.fn()
    [code] $.fn reordering
    [code] Updated method order
    [code] Updated $.* tools
    [code] Safer ibc.target.send()
    [code] More reliable DOM load listener
    
    # April 22, 2025
    [code] prefers-color-scheme set for all
    [code] Scrolling to typing area when necessary
    [code] Improved typeAt w/ support for empy strings
    [code] Improved $.doType
    [code] DRY and Clean Code practices
    
    # April 21, 2025
    [code] Updated docs
    [code] Fixed "cancel-target-promises" async handling
    [chore] Typos
    [code] Fixed "cancel-target-promises" async handling
    [code] More intelligible errors
    [chore] Typos and linting
    [code] Auto-release promises on navigation start
    [code] Automated dom ready check
    
    # April 20, 2025
    [code] DOM Ready checks - baby steps
    [code] $.mouse* cannot steal focus on its own
    [code] Linting incorporates transpiling $
    [code] Decoupled mouse events
    [code] Removed focus from clicks
    
    # April 19, 2025
    [code] Typeable check
    [code] GC for focus timers
    [code] Fixed text append
    [code] Atomic operation for $.type
    [code] Removed blur patching
    [code] Improved typing speed
    [code] General purpose key handler
    
    # April 18, 2025
    [code] More efficient mouse move
    [code] Prepared focus
    [code] Added $.doType
    
    # April 17, 2025
    [code] Cooldown on scroll
    [code] Added $.doClickOn, $.doHoverOn, $.doHoverAt, $.doScrollTo, $.doScroll
    [code] Implemented $.doClickOn
    [code] More efficient mouse move
    
    # April 16, 2025
    [code] Improved $.doClickAt method
    [code] Imnproved release efficiency
    [code] Added pointer png
    [code] Virtual mouse baby steps
    [code] Improved tick-area
    
    # April 15, 2025
    [code] Added $.docGetValue
    [code] WeakMap implementation of bi-directional map for element keys
    
    # April 14, 2025
    [code] Improved query filte...

---
+++ dist/ssg/_next/static/ZJKXxlXakgFS524AmI1F9/_buildManifest.js
@@ -0,0 +1,17 @@
+/*!
+ * @architect Mark Jivko <mark@oglama.com>
+ * @copyright © 2024-2025 Oglama https://oglama.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+self.__BUILD_MANIFEST=function(s,e,c,t){return{__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":[s,e,c,"static/chunks/569-1d94e36908a3d49a.js",t,"static/chunks/pages/index-f52cb5da8884cd92.js"],"/401":["static/chunks/pages/401-83d7d5cf2d8e5ac4.js"],"/404":["static/chunks/pages/404-38903a01bfc8d823.js"],"/500":["static/chunks/pages/500-71919e04c7ffefc3.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],"/_offline":["static/chunks/pages/_offline-34198a61a2499178.js"],"/login":[e,"static/chunks/pages/login-5a63ee9d9ec42ecf.js"],"/source-code":[s,e,c,"static/chunks/524-10bb37bd5be394d7.js",t,"static/chunks/pages/source-code-ee76ca26cd6eabb9.js"],"/test":[s,"static/chunks/pages/test-866f4b6a24d37071.js"],sortedPages:["/","/401","/404","/500","/_app","/_error","/_offline","/login","/source-code","/test"]}}("static/chunks/904-26740f7859bde7f0.js","static/chunks/693-ea892adf950891fa.js","static/chunks/830-d80c3f9197dd04f8.js","static/chunks/629-91a461e04d8c8fe2.js"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
