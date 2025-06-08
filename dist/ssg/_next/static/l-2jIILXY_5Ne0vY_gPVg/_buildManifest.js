3980e58a549a92db5d67bf4a7fd3257bebb4d9cd
Oglama <181064963+oglama@users.noreply.github.com>
2025-06-08 16:55:16 +0300

    [update] Version 0.0.100
    # June 8, 2025
    [code] Uniform CTX and IBC status signaling
    [code] Added selected function flag in SCE
    [code] Improved $.doSelect

---
+++ dist/ssg/_next/static/l-2jIILXY_5Ne0vY_gPVg/_buildManifest.js
@@ -0,0 +1,17 @@
+/*!
+ * @architect Mark Jivko <mark@oglama.com>
+ * @copyright © 2024-2025 Oglama https://oglama.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+self.__BUILD_MANIFEST=function(s,c,e,a,t){return{__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":[s,c,e,a,"static/chunks/8-386d262c59b5d3c3.js",t,"static/chunks/pages/index-b99b019656e3f3e3.js"],"/404":["static/chunks/pages/404-6555439f0ebbaca5.js"],"/500":["static/chunks/pages/500-38ec18e134d5f66b.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],"/_offline":["static/chunks/pages/_offline-34198a61a2499178.js"],"/home":["static/chunks/pages/home-cfed0a86221e0410.js"],"/home/test":[s,c,a,"static/chunks/945-b1c849c55b815f5c.js","static/chunks/pages/home/test-a1a1a7da94e36969.js"],"/login":[s,"static/chunks/pages/login-afb338e172e94890.js"],"/source-code":[s,c,e,"static/chunks/103-c48936b4252a8a0a.js",t,"static/chunks/pages/source-code-9727e8680c965b60.js"],sortedPages:["/","/404","/500","/_app","/_error","/_offline","/home","/home/test","/login","/source-code"]}}("static/chunks/901-a879241edc23de24.js","static/chunks/884-926c8bdbfa22a1de.js","static/chunks/469-ea2cf913c8d0ce83.js","static/chunks/355-8b8a93a594e42336.js","static/chunks/605-bac80143cb6aa9b5.js"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
